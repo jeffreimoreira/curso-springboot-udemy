c1944674dbf0a515c245ba3c7bfd3c18ce961a3c
Jeffrei S Moreira <jeffreism@gmail.com>
2021-03-18 19:53:27 -0300

    OrderStatus enum

--- src/main/java/com/jsm/cursoudemy/config/TestConfig.java
+++ src/main/java/com/jsm/cursoudemy/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.jsm.cursoudemy.entities.Order;
 import com.jsm.cursoudemy.entities.User;
+import com.jsm.cursoudemy.entities.enums.OrderStatus;
 import com.jsm.cursoudemy.repositories.OrderRepository;
 import com.jsm.cursoudemy.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "9888888", "12345");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777", "12345");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
