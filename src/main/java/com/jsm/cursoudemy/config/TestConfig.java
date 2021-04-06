bd962c1573be9f2b36d53d827be3dd528371ea6b
Jeffrei S Moreira <jeffreism@gmail.com>
2021-04-06 19:29:05 -0300

    Payment, one-to-one association

--- src/main/java/com/jsm/cursoudemy/config/TestConfig.java
+++ src/main/java/com/jsm/cursoudemy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jsm.cursoudemy.entities.Category;
 import com.jsm.cursoudemy.entities.Order;
 import com.jsm.cursoudemy.entities.OrderItem;
+import com.jsm.cursoudemy.entities.Payment;
 import com.jsm.cursoudemy.entities.Product;
 import com.jsm.cursoudemy.entities.User;
 import com.jsm.cursoudemy.entities.enums.OrderStatus;
@@ -76,6 +77,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 	}
 	
 }
